7ac6755d328efec85e40094e5e4ed7e6626c71cf
Kevin <kevin@naivesystems.com>
2022-05-12 11:37:59 +0800

    add fopen

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "test.h"
 
 int main() {
 	typedef int local_type;
 	int a = 6 / 0;
 	g();
+	FILE * fp;
+ 
+        fp = fopen ("file.txt", "w+");
+
 	printf("sss");
 	return 0;
 	int b = 0;
