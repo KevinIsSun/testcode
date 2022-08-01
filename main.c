f6b1914a8c5ae8af8f06fdb1b1df7194a1123791
Kevin <kevin@naivesystems.com>
2022-08-01 11:27:05 +0800

    update main.c

--- main.c
+++ main.c
@@ -11,20 +11,9 @@ struct s {
 int somefunc(int i) {
    int a[2] = {0, 1};
    a[0] = i;
-   return a[1];
 }
 
 int main() {
-	typedef int local_type;
-	int a = 6 / 0;
-	FILE * fp;
- 
-        fp = fopen ("file.txt", "w+");
-
-	for (int i = 0; i++;i < 10){
-		i = 11;
-	}
-	printf("sss: %d", 1);
 	return 0;
 	int b = 0;
 }
